1e7e9e74e0480b50de266146dbe31d168c3abc6b
uncle-lv <uncle.lv@outlook.com>
2021-08-20 19:41:25 +0800

    ✨ feat: add a logger

---
+++ log/log.go
@@ -0,0 +1,54 @@
+package log
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+var (
+	errorLog = log.New(os.Stdout, "\033[31m[error]\033[0m ", log.LstdFlags|log.Lshortfile)
+	warnLog  = log.New(os.Stdout, "\033[33m[warning]\033[0m ", log.LstdFlags|log.Lshortfile)
+	infoLog  = log.New(os.Stdout, "\033[32m[info]\033[0m ", log.LstdFlags|log.Lshortfile)
+	loggers  = []*log.Logger{errorLog, warnLog, infoLog}
+
+	mu sync.Mutex
+)
+
+var (
+	Error  = errorLog.Println
+	Errorf = errorLog.Printf
+	Warn   = warnLog.Println
+	Warnf  = warnLog.Printf
+	Info   = infoLog.Println
+	Infof  = infoLog.Printf
+)
+
+const (
+	InfoLevel = iota
+	WarnLevel
+	ErrorLevel
+	Disabled
+)
+
+func SetLevel(level int) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for _, logger := range loggers {
+		logger.SetOutput(os.Stdout)
+	}
+
+	if ErrorLevel < level {
+		errorLog.SetOutput(ioutil.Discard)
+	}
+
+	if WarnLevel < level {
+		warnLog.SetOutput(ioutil.Discard)
+	}
+
+	if InfoLevel < level {
+		infoLog.SetOutput(ioutil.Discard)
+	}
+}
